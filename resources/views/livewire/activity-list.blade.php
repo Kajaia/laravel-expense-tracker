288da2b5b44b59e7479b9a6a29b0acdb009e06f0
Kajaia <lashakajaia31@gmail.com>
2022-09-18 11:28:39 +0400

    Showing all activities for category collapse

--- resources/views/livewire/activity-list.blade.php
+++ resources/views/livewire/activity-list.blade.php
@@ -24,31 +24,31 @@
                 </span>
             </div>
         </div>
-        @forelse($category->activities as $activity)
         <div id="details-{{ $category->id }}" class="px-4 py-2 bg-light rounded-2 mt-2 mb-3 d-none">
-                <div class="d-flex align-items-center justify-content-between">
-                    <div>
-                        <small class="{{ $category->type === 'add' ? 'text-success' : 'text-danger' }}">
-                            {{ $activity->amount . '₾' }}
-                            @if($activity->note)
-                            <span class="tiny-text text-dark">
-                                ({{ $activity->note }})
-                            </span>
-                            @endif
-                        </small>
-                    </div>
-                    <div>
-                        <small>{{ Carbon\Carbon::parse($activity->created_at)->format('d M, Y') }}</small>
-                    </div>
+            @forelse($category->activities as $activity)
+            <div class="d-flex align-items-center justify-content-between">
+                <div>
+                    <small class="{{ $category->type === 'add' ? 'text-success' : 'text-danger' }}">
+                        {{ $activity->amount . '₾' }}
+                        @if($activity->note)
+                        <span class="tiny-text text-dark">
+                            ({{ $activity->note }})
+                        </span>
+                        @endif
+                    </small>
                 </div>
-                @if(!$loop->last)
-                <hr class="m-0 p-0 text-light" />
-                @endif
+                <div>
+                    <small>{{ Carbon\Carbon::parse($activity->created_at)->format('d M, Y') }}</small>
+                </div>
+            </div>
+            @if(!$loop->last)
+            <hr class="m-0 p-0 text-dark" />
+            @endif
+            @empty
+            @endforelse
         </div>
-        @empty
-        @endforelse
         @if(!$loop->last)
-        <hr id="divider-{{ $category->id }}" class="my-2 p-0 text-dark" />
+        <hr id="divider-{{ $category->id }}" class="my-2 p-0 text-secondary" />
         @endif
     @endforeach
 </div>
