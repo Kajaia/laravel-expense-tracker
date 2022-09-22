39d80875f3574be1942cf9a71a8087f269ff2ed1
Kajaia <lashakajaia31@gmail.com>
2022-09-22 13:13:02 +0400

    Added sms helper and config

---
+++ config/sms.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    
+    /**
+     * SMS Service from sender.ge
+     */
+    'sender' => [
+        'url' => env('SENDER_URL', ''),
+        'api_key' => env('SENDER_API', '')
+    ]
+    
+];
