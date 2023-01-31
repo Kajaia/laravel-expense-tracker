c5c26a3a9ca11bec475d1554e0448d804e56e916
Kajaia <lashakajaia31@gmail.com>
2023-01-31 14:03:17 +0200

    Updated GetExpensesCountAction

--- app/Actions/Category/GetExpensesCountAction.php
+++ app/Actions/Category/GetExpensesCountAction.php
@@ -3,31 +3,26 @@
 namespace App\Actions\Category;
 
 use App\Models\Activity;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class GetExpensesCountAction
 {
     public function __construct(
         protected Activity $model
-    ) {}
+    ) {
+    }
 
     public function __invoke(?string $from = null, ?string $to = null): float
     {
         return $this->model::with('category')
-            ->whereHas('category', function($query) {
+            ->whereHas('category', function ($query) {
                 $query->where('type', 'subtract');
             })
             ->where('user_id', Auth::user()->id)
-            ->when($from && $to, function($query) use ($from, $to) {
+            ->when($from && $to, function ($query) use ($from, $to) {
                 $query->whereBetween('created_at', [
-                    $from.' 00:00:00',
-                    $to.' 23:59:59'
-                ]);
-            }, function($query) {
-                $query->whereBetween('created_at', [
-                    Carbon::now()->subMonth()->format('Y-m-d').' 00:00:00',
-                    Carbon::now()->format('Y-m-d H:i:s')
+                    $from . ' 00:00:00',
+                    $to . ' 23:59:59'
                 ]);
             })->sum('amount');
     }
